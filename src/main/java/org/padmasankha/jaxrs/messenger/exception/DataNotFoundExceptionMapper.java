ca0d971f2f31dab92ef39fac5cb0ac9c1e93e684
padmasankha <padmasankha@yahoo.com>
2017-03-27 13:55:33 +0530

    Add Error handling classes

---
+++ src/main/java/org/padmasankha/jaxrs/messenger/exception/DataNotFoundExceptionMapper.java
@@ -0,0 +1,22 @@
+package org.padmasankha.jaxrs.messenger.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.padmasankha.jaxrs.messenger.model.ErrorMessage;
+
+@Provider
+public class DataNotFoundExceptionMapper implements ExceptionMapper<DataNotFoundException> {
+
+	@Override
+	public Response toResponse(DataNotFoundException ex) {
+		
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 404, "http://test.com/link");
+		return Response.status(Status.NOT_FOUND)
+					   .entity(errorMessage)
+					   .build();
+	}
+
+}
